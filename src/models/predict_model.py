1ebd2ab08b54c9680c1d8de9b2a79ca30319a8b7
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-13 22:10:47 -0400

    Update predict_model.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -3,3 +3,9 @@ from .model import Summarization
 def predict_model(text):
     """
     Predict the summary of the given text.
+    """
+    model = Summarization()
+    model.load_model()
+    pre_summary = model.predict(text)
+    return pre_summary
+    
