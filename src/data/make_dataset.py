8cab515a04cca3b42b5d50ca4d50b109e14cac01
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-13 22:05:39 -0400

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,3 +6,4 @@ def make_dataset(dataset='cnn_dailymail', split='train', version="3.0.0"):
     """make dataset for summarisation"""
     dataset = load_dataset(dataset, split=split, script_version=version)
     df = pd.DataFrame()
+    df['input_text'] = dataset['concepts']
