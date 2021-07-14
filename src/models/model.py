187bb521a894523251d72cf4e04f22a6978cf23f
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-13 22:07:10 -0400

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -13,3 +13,5 @@ import pytorch_lightning as pl
 from pytorch_lightning.loggers import MLFlowLogger
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning import LightningDataModule
+from pytorch_lightning import LightningModule
