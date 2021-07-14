eb71d142efd4ed089a67cce7b9c9ee14dff20f22
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-13 22:05:43 -0400

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,3 +7,4 @@ def make_dataset(dataset='cnn_dailymail', split='train', version="3.0.0"):
     dataset = load_dataset(dataset, split=split, script_version=version)
     df = pd.DataFrame()
     df['input_text'] = dataset['concepts']
+    df['output_text'] = dataset['target']
