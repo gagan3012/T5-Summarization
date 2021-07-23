5ecdcfb51284cbbd232acd1b7e4e04922d251a11
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-23 16:09:58 -0700

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -259,6 +259,7 @@ class Summarization:
             self.tokenizer = T5Tokenizer.from_pretrained(f"{model_name}")
             self.model = T5ForConditionalGeneration.from_pretrained(
                 f"{model_name}", return_dict=True
+            )
 
     def train(
             self,
