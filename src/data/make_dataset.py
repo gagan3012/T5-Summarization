e0a340d67339dbe50e4077ca6a0a96675d0ff2be
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-23 16:07:35 -0700

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,7 +6,6 @@ def make_dataset(dataset='cnn_dailymail', split='train'):
     """make dataset for summarisation"""
     dataset = load_dataset(dataset, '3.0.0', split=split)
     df = pd.DataFrame()
-    return df
     df['article'] = dataset['article']
 
 if __name__ == '__main__':
