a0251a7a8b585c0bac972e20d38e9c673ab78c82
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-23 16:07:19 -0700

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,7 +2,6 @@ from datasets import load_dataset
 import pandas as pd
 
 
-def make_dataset(dataset='cnn_dailymail', split='train', version="3.0.0"):
 def make_dataset(dataset='cnn_dailymail', split='train'):
     """make dataset for summarisation"""
     dataset = load_dataset(dataset, split=split, script_version=version)
