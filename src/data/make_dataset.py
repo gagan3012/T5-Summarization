953125551c849837a1c54db24a717c4a99102f2f
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-25 11:12:45 -0700

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -9,7 +9,7 @@ def make_dataset(dataset='cnn_dailymail', split='train'):
     df = pd.DataFrame()
     df['article'] = dataset['article']
     df['highlights'] = dataset['highlights']
-    df.to_csv('C:/Users/gbhat/Documents/GitHub/summarization/data/raw/{}.csv'.format(split))
+    df.to_csv('data/raw/{}.csv'.format(split))
 
 
 if __name__ == '__main__':
