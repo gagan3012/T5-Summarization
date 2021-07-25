66b26f5eab7f5970f272d04ecd03ad3c06817c7b
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-25 11:14:46 -0700

    Update evaluate_model.py

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -15,7 +15,7 @@ def evaluate_model():
     test_df = pd.load_csv('data/processed/test.csv')
     model = Summarization()
     model.load_model()
-    results = model.evaluate(test_df=test_df,metrics="rouge")
+    results = model.evaluate(test_df=test_df, metrics=params['metric'])
     with dagshub.dagshub_logger() as logger:
         logger.log_metrics(results)
     return results
