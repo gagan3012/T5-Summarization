1b154c5044a3f924febd0f328bcbee417902c813
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-25 11:13:22 -0700

    Update process_data.py

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def process_data(split='train'):
+
     df.columns = ['Unnamed: 0', 'input_text', 'output_text']
     print(df.columns)
     df.to_csv('C:/Users/gbhat/Documents/GitHub/summarization/data/processed/{}.csv'.format(split))
