dc0c603de62ec4297757cabfa72cfec51c5dbded
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-25 11:16:05 -0700

    Update predict_model.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -19,6 +19,6 @@ def predict_model(text):
 
 
 if __name__ == '__main__':
-    text = make_dataset(split="test")['input_text'][0]
+    text = pd.load_csv('data/processed/test.csv')['input_text'][0]
     pre_summary = predict_model(text)
     print(pre_summary)  
