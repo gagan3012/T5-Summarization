e1f665f86a820793ce46dd051de94740d7cb9aeb
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-28 10:02:32 -0700

    Update process_data.py

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -7,6 +7,7 @@ def process_data(split='train'):
 
     with open("params.yml") as f:
         params = yaml.safe_load(f)
+
     df = pd.read_csv('data/raw/{}.csv'.format(split))
     df.columns = ['Unnamed: 0', 'input_text', 'output_text']
     df.to_csv('data/processed/{}.csv'.format(split))
