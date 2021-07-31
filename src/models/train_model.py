80a254f191bf24fa287038f62083b17aa6161685
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-31 01:04:02 -0700

    Update train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -17,7 +17,6 @@ def train_model():
     train_df = pd.read_csv("data/processed/train.csv")
     eval_df = pd.read_csv("data/processed/validation.csv")
 
-    eval_df = eval_df.sample(frac=params['split'], replace=True, random_state=1)
 
     model = Summarization()
     model.from_pretrained(model_type=params['model_type'], model_name=params['model_name'])
