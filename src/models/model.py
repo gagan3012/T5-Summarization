b3810dd7edfd854bde7a159d50bc8dd82c17893a
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-31 01:05:58 -0700

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -150,9 +150,9 @@ class PLDataModule(LightningDataModule):
         )
 
     def val_dataloader(self):
-        """ validation dataloader """
+        """validation dataloader"""
         return DataLoader(
-            self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers
+            self.test_dataset,
         )
 
 
