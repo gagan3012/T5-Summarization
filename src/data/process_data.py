fbbe319eccf72b0d8bff498e309061acd38415dc
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-31 01:02:25 -0700

    Update process_data.py

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -9,6 +9,7 @@ def process_data(split="train"):
         params = yaml.safe_load(f)
 
     df = pd.read_csv("data/raw/{}.csv".format(split))
+    df.columns = ["Unnamed: 0", "input_text", "output_text"]
     if os.path.exists("data/raw/{}.csv".format(split)):
         os.remove("data/raw/{}.csv".format(split))
     df.to_csv('data/processed/{}.csv'.format(split))
