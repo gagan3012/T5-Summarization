bd01d6585273847532e25c9a41273c4e5066cfa6
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-31 01:03:34 -0700

    Update predict_model.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -12,7 +12,7 @@ def predict_model(text):
         params = yaml.safe_load(f)
 
     model = Summarization()
-    model.load_model(model_type=params['model_type'], model_dir=params['model_dir'])
+    model.load_model(model_type=params["model_type"], model_dir=params["model_dir"])
     pre_summary = model.predict(text)
     return pre_summary
 
