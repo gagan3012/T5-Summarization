c63847db7616de45327c5bc263aaf602b3f287a0
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-31 01:04:18 -0700

    Update train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -29,6 +29,7 @@ def train_model():
     model.save_model(model_dir=params['model_dir'])
 
     with open('wandb/latest-run/files/wandb-summary.json') as json_file:
+        model_type=params["model_type"], model_name=params["model_name"]
         data = json.load(json_file)
 
     with open('reports/training_metrics.txt', 'w') as fp:
