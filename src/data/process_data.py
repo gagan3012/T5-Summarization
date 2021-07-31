93ebdf4a0679f998f1766307b6417bd4a3f82fa0
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-31 01:02:21 -0700

    Update process_data.py

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -8,7 +8,6 @@ def process_data(split="train"):
     with open("params.yml") as f:
         params = yaml.safe_load(f)
 
-    df = df.sample(frac=params['split'], replace=True, random_state=1)
     if os.path.exists("data/raw/{}.csv".format(split)):
         os.remove("data/raw/{}.csv".format(split))
     df.to_csv('data/processed/{}.csv'.format(split))
