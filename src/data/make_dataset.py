bc51ebd9e80d812dd2f554155566cce4534fb5fd
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-31 01:01:42 -0700

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -11,6 +11,7 @@ def make_dataset(dataset="cnn_dailymail", split="train"):
         os.makedirs("data/raw")
     dataset = load_dataset(dataset, "3.0.0", split=split)
     df = pd.DataFrame()
+    df["article"] = dataset["article"]
     df["highlights"] = dataset["highlights"]
     df.to_csv("data/raw/{}.csv".format(split))
 
