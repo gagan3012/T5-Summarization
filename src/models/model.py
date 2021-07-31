2c57b147fad09f39f691015919cfba0acc63c9f4
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-31 01:05:45 -0700

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -141,7 +141,7 @@ class PLDataModule(LightningDataModule):
         )
 
     def test_dataloader(self):
-        """ test dataloader """
+        """test dataloader"""
         return DataLoader(
             self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers
         )
