3f9b6dfaf8a6ced4c2745b9312959736611af3ef
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-31 01:03:03 -0700

    Update evaluate_model.py

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -13,6 +13,7 @@ def evaluate_model():
     with open("params.yml") as f:
         params = yaml.safe_load(f)
 
+    test_df = pd.read_csv("data/processed/test.csv")[:25]
     model = Summarization()
     model.load_model(model_type=params['model_type'], model_dir=params['model_dir'])
     results = model.evaluate(test_df=test_df, metrics=params['metric'])
