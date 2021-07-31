b11282fefac62fb6ec1175e22e53267e8c4e27fd
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-31 01:03:06 -0700

    Update evaluate_model.py

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -15,7 +15,6 @@ def evaluate_model():
 
     test_df = pd.read_csv("data/processed/test.csv")[:25]
     model = Summarization()
-    model.load_model(model_type=params['model_type'], model_dir=params['model_dir'])
     results = model.evaluate(test_df=test_df, metrics=params['metric'])
 
     with open('reports/metrics.txt', 'w') as fp:
