06eee644e6c1b0a3b9236996a8db3a2d7a96f940
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-04 10:28:04 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -9,3 +9,4 @@ from src.models.predict_model import predict_model  # noqa: F401
 from src.models.train_model import train_model  # noqa: F401
 from src.visualization.visualize import visualize  # noqa: F401
 
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))  # noqa: F401
