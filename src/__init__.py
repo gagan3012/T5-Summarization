fd4d73f4da012662c924a17b68e60eae4446f079
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-04 00:13:05 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,4 @@ from src.models.evaluate_model import evaluate_model
 from src.models.predict_model import predict_model
 from src.models.train_model import train_model
 from src.models.model import Summarization
+from src.data.make_dataset import make_dataset
