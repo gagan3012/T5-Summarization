33da28e1f3058c13d387a9277ca4a042a7345a49
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-04 10:28:26 -0700

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,2 @@
-from .predict_model import predict_model
 from .evaluate_model import evaluate_modelfrom .train_model import train_model  # noqa: F401
 from .model import Summarization  # noqa: F401
