9152d6aa428b0c65838cecdaea2ce521516b3a1f
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-04 10:27:46 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,4 @@ import sys  # noqa: F401
 
 from src.data.make_dataset import make_dataset  # noqa: F401
 from src.data.process_data import process_data  # noqa: F401
+from src.models.evaluate_model import evaluate_model  # noqa: F401
