a16f9c78ba6e4b1c0522b810bc437e28d866a3ba
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-04 10:27:14 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,3 @@
-from src.models.model import Summarization
 from src.data.make_dataset import make_dataset
 from src.data.process_data import process_data
 from src.visualization.visualize import visualize
