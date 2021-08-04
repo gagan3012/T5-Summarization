9499035682835d6e567113f015e09175d0184e7e
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-04 00:13:12 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,3 +5,4 @@ from src.models.model import Summarization
 from src.data.make_dataset import make_dataset
 from src.data.process_data import process_data
 from src.visualization.visualize import visualize
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
