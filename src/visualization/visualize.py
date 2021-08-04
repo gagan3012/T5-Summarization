0ffaf4912ad16d6d1ddf799fc026c941d67c6545
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-04 10:31:27 -0700

    Update visualize.py

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -7,7 +7,7 @@ from models import predict_model
 def visualize():
     st.write("# Summarization  UI")
     st.markdown(
-        '''
+        """
         *For additional questions and inquiries, please contact **Gagan Bhatia** via [LinkedIn](
         https://www.linkedin.com/in/gbhatia30/) or [Github](https://github.com/gagan3012).*
         ''')
