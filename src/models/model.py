c201811c0aa576c3c3c6dd194d43fe3648f44640
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-04 10:30:51 -0700

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -561,3 +561,6 @@ class Summarization:
         del hf_password
         model_url = HfApi().create_repo(token=token, name=model_name, exist_ok=True)
         model_repo = Repository(
+            "./model",
+            clone_from=model_url,
+            use_auth_token=token,
