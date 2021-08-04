108d891af22a3251a7ceb8b68e3861d4afb1673a
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-04 10:30:54 -0700

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -564,3 +564,5 @@ class Summarization:
             "./model",
             clone_from=model_url,
             use_auth_token=token,
+            git_email=f"{hf_username}@users.noreply.huggingface.co",
+            git_user=hf_username,
