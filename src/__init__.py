9199694c688c6acffa0416b3bd1bf243632c1ae1
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-04 10:28:01 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -8,3 +8,4 @@ from src.models.model import Summarization  # noqa: F401
 from src.models.predict_model import predict_model  # noqa: F401
 from src.models.train_model import train_model  # noqa: F401
 from src.visualization.visualize import visualize  # noqa: F401
+
