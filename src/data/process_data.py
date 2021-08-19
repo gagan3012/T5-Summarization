fef1790fff3df15aabc147b224b6f5b2d344f310
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-19 12:20:37 -0700

    Update process_data.py

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -5,7 +5,7 @@ import yaml
 def process_data(frac=0.1, split="train"):
     df = pd.read_csv("data/raw/{}.csv".format(split))
     df.columns = ["Unnamed: 0", "input_text", "output_text"]
-        os.remove("data/raw/{}.csv".format(split))
+    df_new = df.sample(frac=frac, replace=True, random_state=1)
     df_new.to_csv("data/processed/{}.csv".format(split))
 
 
