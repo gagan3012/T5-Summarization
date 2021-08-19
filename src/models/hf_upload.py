07f6f2cf9e9351daf32b35714d2fcf555dbd275e
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-19 12:23:48 -0700

    Update hf_upload.py

--- src/models/hf_upload.py
+++ src/models/hf_upload.py
@@ -11,3 +11,7 @@ from huggingface_hub import HfApi, Repository
 def upload(model_to_upload, model_name):
     hf_username = input("Enter your HuggingFace username:")
     hf_token = getpass("Enter your HuggingFace token:")
+    model_url = HfApi().create_repo(token=hf_token, name=model_name, exist_ok=True)
+    model_repo = Repository(
+        "./hf_model",
+        clone_from=model_url,
