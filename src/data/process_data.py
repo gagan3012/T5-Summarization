7599e2df279850a166f64123215d5495ab2c12eb
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-19 12:20:33 -0700

    Update process_data.py

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -6,7 +6,7 @@ def process_data(frac=0.1, split="train"):
     df = pd.read_csv("data/raw/{}.csv".format(split))
     df.columns = ["Unnamed: 0", "input_text", "output_text"]
         os.remove("data/raw/{}.csv".format(split))
-    df.to_csv("data/processed/{}.csv".format(split))
+    df_new.to_csv("data/processed/{}.csv".format(split))
 
 
 if __name__ == "__main__":
