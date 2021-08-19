f6d676aa0d36a22c0a59f2bd0adf59a33180ee47
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-19 12:27:03 -0700

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -10,7 +10,7 @@ from transformers import (
 )
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
-from pytorch_lightning.loggers import MLFlowLogger, WandbLogger
+from dagshub.pytorch_lightning import DAGsHubLogger
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning import LightningDataModule
