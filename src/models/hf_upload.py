5f4533735fabda0086649852d44cc797dc983036
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-19 12:23:43 -0700

    Update hf_upload.py

--- src/models/hf_upload.py
+++ src/models/hf_upload.py
@@ -8,3 +8,6 @@ from model import Summarization
 from huggingface_hub import HfApi, Repository
 
 
+def upload(model_to_upload, model_name):
+    hf_username = input("Enter your HuggingFace username:")
+    hf_token = getpass("Enter your HuggingFace token:")
